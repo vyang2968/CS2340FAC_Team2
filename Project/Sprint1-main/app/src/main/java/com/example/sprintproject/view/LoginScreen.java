25a78d2a557b5371c628fef7a15499d8e998b2ff
ahoque4 <hoque.azman@gmail.com>
2024-10-03 17:12:31 -0400

    Added WelcomeScreen, temp LoginScreen, fixed MVVM

--- Project/Sprint1-main/app/src/main/java/com/example/sprintproject/view/LoginScreen.java
+++ Project/Sprint1-main/app/src/main/java/com/example/sprintproject/view/LoginScreen.java
@@ -1,16 +1,14 @@
 package com.example.sprintproject.view;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
 import com.example.sprintproject.R;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login_screen);
     }
 }

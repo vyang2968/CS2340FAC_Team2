23d362de97d390502ff010a02a20cdd2d6639ebc
AveenaSingh <152574824+AveenaSingh@users.noreply.github.com>
2024-10-06 20:44:00 -0400

    Java and xml files for placeholder screens

---
+++ Project/Sprint1-main/app/src/main/java/com/example/sprintproject/view/Accommodation.java
@@ -0,0 +1,14 @@
+package com.example.sprintproject.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.android_assignment_fa_2024.R;
+
+public class Accommodation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accomodation);
+    }
+}

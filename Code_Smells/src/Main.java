3f196be529e35184c8a92853f32bc9517d9ffc4b
vyang <v.yang2968@gmail.com>
2024-11-03 16:21:01 -0500

    rename main to Main

--- Code_Smells/src/Main.java
+++ Code_Smells/src/Main.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class main {
+public class Main {
 	public static void main(String[] args) {
         Item item1 = new Item("Book", 20, 1, new FixedDiscount(), 5, false);
         Item item2 = new TaxableItem("Laptop", 1000, 1, new PercentDiscount(), 0.1, false);

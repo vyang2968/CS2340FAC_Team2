64a8182a1d0793b2228e4ff19f737f576f4ed15e
Eman3600 <real.eman3600@gmail.com>
2024-11-08 18:20:35 -0500

    Fix Crashes

--- Project/Sprint3-main/app/src/main/java/com/example/sprintproject/view/AccommodationScreen.java
+++ Project/Sprint3-main/app/src/main/java/com/example/sprintproject/view/AccommodationScreen.java
@@ -2,15 +2,30 @@ package com.example.sprintproject.view;
 
 
 import android.os.Bundle;
-import com.example.sprintproject.R;
+
+import androidx.activity.EdgeToEdge;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.sprintproject.BR;
+import com.example.sprintproject.databinding.ActivityAccommodationBinding;
+import com.example.sprintproject.viewmodel.AccommodationViewModel;
 
 public class AccommodationScreen extends NavBarScreen {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_accommodation);
+        EdgeToEdge.enable(this);
+
+        ActivityAccommodationBinding binding =
+                ActivityAccommodationBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         setupNavBar();
+
+        AccommodationViewModel accommodationViewModel =
+                new ViewModelProvider(this).get(AccommodationViewModel.class);
+        binding.setVariable(BR.accoViewModel, accommodationViewModel);
+        binding.setLifecycleOwner(this);
     }
 }

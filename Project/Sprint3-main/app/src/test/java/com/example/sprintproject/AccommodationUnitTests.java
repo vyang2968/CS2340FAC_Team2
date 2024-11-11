bbc929189e324ad708bfe15590f40e6c6d0867da
AveenaSingh <152574824+AveenaSingh@users.noreply.github.com>
2024-11-11 00:13:18 -0500

    Accommodation Unit Tests

--- Project/Sprint3-main/app/src/test/java/com/example/sprintproject/AccommodationUnitTests.java
+++ Project/Sprint3-main/app/src/test/java/com/example/sprintproject/AccommodationUnitTests.java
@@ -8,6 +8,8 @@ import com.example.sprintproject.model.Accommodation.RoomType;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Date;
+
 public class AccommodationUnitTests {
 
     private Accommodation accommodation;
@@ -29,5 +31,19 @@ public class AccommodationUnitTests {
         Accommodation.RoomType roomType = RoomType.SUITE;
         accommodation.setRoomType(roomType);
         assertEquals(roomType, accommodation.getRoomType());
+    }
+
+    @Test
+    public void testCheckoutDate() {
+        Date checkout = new Date(01/01/2033);
+        accommodation.setCheckOutTime(checkout);
+        assertEquals(checkout, accommodation.getCheckOutTime());
+    }
+
+    @Test
+    public void testCheckInDate() {
+        Date checkIn = new Date(01/01/2030);
+        accommodation.setCheckInTime(checkIn);
+        assertEquals(checkIn, accommodation.getCheckOutTime());
     }
 }

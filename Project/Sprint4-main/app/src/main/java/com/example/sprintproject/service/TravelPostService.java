9e913cb31c6d69811247db23d169338e962efd38
vyang <v.yang2968@gmail.com>
2024-11-23 19:43:12 -0500

    travel post db stuff

---
+++ Project/Sprint4-main/app/src/main/java/com/example/sprintproject/service/TravelPostService.java
@@ -0,0 +1,44 @@
+package com.example.sprintproject.service;
+
+import com.example.sprintproject.model.TravelPost;
+import com.example.sprintproject.model.Trip;
+import com.example.sprintproject.model.User;
+import com.example.sprintproject.repository.TravelPostRepositoryImpl;
+import com.example.sprintproject.repository.TripRepositoryImpl;
+import com.example.sprintproject.utils.DataCallback;
+import com.google.android.gms.tasks.Task;
+
+public class TravelPostService {
+    private final TravelPostRepositoryImpl travelPostRepositoryImpl;
+    private static TravelPostService instance;
+    private static final String TAG = "TravelService";
+
+    public static TravelPostService getInstance() {
+        if (instance == null) {
+            instance = new TravelPostService();
+        }
+        return instance;
+    }
+
+    private TravelPostService() {
+        this.travelPostRepositoryImpl = new TravelPostRepositoryImpl();
+    }
+
+    public Task<Void> addTravelPost(TravelPost post) {
+        if (post == null) {
+            throw new IllegalArgumentException("post cannot be null");
+        }
+
+        return travelPostRepositoryImpl.addTravelPost(post);
+    }
+
+    public void getTravelPost(String id, DataCallback<TravelPost> callback) {
+        if (id == null) {
+            throw new IllegalArgumentException("id cannot be null");
+        } else if (id.isEmpty()) {
+            throw new IllegalArgumentException("id cannot be empty");
+        }
+
+        travelPostRepositoryImpl.getTravelPostById(id, callback);
+    }
+}
